e8a42397da865503ccb5e840ba3ccfc90a0b2221
Idris adekunle <idrisadekunle726@gmail.com>
2022-08-01 15:29:37 +0100

    client and info done

--- client-details-Ade/details-info.js
+++ client-details-Ade/details-info.js
@@ -7,9 +7,19 @@ const  detailsDiv = document.getElementsByClassName('main-details')[0]
 detailBtn.addEventListener('click', function(event){
    infoDiv.classList.add('none')
    detailsDiv.classList.remove('none')
+detailBtn.style.border =' 1px solid #407BFF'
+detailBtn.style.color =' #407BFF'
+infoBtn.style.color =' black'
+
+infoBtn.style.border ='none'
 
 })
 infoBtn.addEventListener('click', function(event){
    detailsDiv.classList.add('none')
    infoDiv.classList.remove('none')
+   infoBtn.style.border =' 1px solid #407BFF'
+   detailBtn.style.border ='none'
+   infoBtn.style.color =' #407BFF'
+   detailBtn.style.color =' black'
+
 })
